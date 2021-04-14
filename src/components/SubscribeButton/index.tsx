412ac74ce34c5d79bb86463934ed2ee52e4f069f
Olavo Licursi <55329614+olavo-licursi@users.noreply.github.com>
2021-04-14 18:51:20 -0300

    Preview do post

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -2,6 +2,7 @@ import { useSession, signIn } from 'next-auth/client';
 import { api } from '../../services/api';
 import { getStripeJs } from '../../services/stripe-js';
 import styles from './styles.module.scss';
+import { useRouter } from 'next/router';
 
 interface SubscribeButtonProps {
     priceId: string;
@@ -9,12 +10,19 @@ interface SubscribeButtonProps {
 
 export function SubscribeButton({priceId}: SubscribeButtonProps) {
     const [session] = useSession();
+    const router = useRouter();
 
     async function handleSubscribe() {
         if(!session) {
             signIn('github')
             return;
         }
+
+        if(session.activeSubscription) {
+            router.push('/posts');
+            return;
+        }
+
         //criação da checkout session
         try {
             const response = await api.post('/subscribe')
